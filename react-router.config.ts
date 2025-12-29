88af79ab7e9ab3647ec2f99d26aa877b04bf830b
Tanner Perrien <weslee3@gmail.com>
2025-12-28 22:44:05 -0500

    Adjust react router basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,8 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false,
+  basename: process.env.NODE_ENV === "production" ? "/qr-ky" : undefined,
   // Pre-render the home route at build time
   async prerender() {
     return ["/"];
